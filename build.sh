ab84529bdbe82d16dfa5794f492a1e3ba55b482e
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2022-08-10 13:50:24 +0000

    A huge update
    This time, I'm really dedicating myself to my personal website. I have a
    "nearly complete" draft and now I'm restructuring everything so that I
    have a POSIX Shell script that will build the website, so I can more
    easily work with the concept of templates.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# build.sh — Script for build my site
+# ======================================================================
+#
+# Author:
+# ----------------------------------------------------------------------
+# Heinrich van Tol Taver
+#
+# Commentary:
+# ----------------------------------------------------------------------
+# Originally meant to be an Emacs Lisp code, right now I'll be doing
+# in POSIX shell just because it's what I know. Probably Ill be
+# rewriting this in a later stage.
+#
+# As with everything else, since I want to do all by myself, I will
+# not be using Elmstatic, Hugo or anythin like that.
+#
+#
+# TODO:
+# ----------------------------------------------------------------------
+
+main() {
+  echo "Hello world!"
+}
+
+# Entry point
+# ======================================================================
+
+main
