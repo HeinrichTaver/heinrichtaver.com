5cad91d2b760580b8d839b0a5d4600e1ff690c68
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2025-08-09 17:43:18 +0000

    feat: added Astro Sitemap; ready for production
    As with any good framework, Astro also has a feature, via integration,
    to automatically generate *Sitemap* files.
    
    Now the website is complete and ready to be put into production.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import fs from "fs";
+
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://heinrichtaver.com",
+
   image: {
     layout: "constrained",
     responsiveStyles: true,
-  }
+  },
+
+  integrations: [sitemap({
+    serialize(item) {
+      switch (new URL(item.url).pathname) {
+      case "/":
+        item.lastmod = fs.statSync("./src/pages/index.md").mtime.toISOString();
+        break;
+      case "/now/":
+        item.lastmod = fs.statSync("./src/pages/now.md").mtime.toISOString();
+        break;
+      case "/blog/":
+        item.lastmod = fs.statSync("./src/pages/blog.astro").mtime.toISOString();
+        break;
+      default:
+        const page = `./src/pages${new URL(item.url).pathname}`;
+
+        if (/\/blog\/.*/.test(page)) {
+          item.lastmod = fs.statSync(`${page.slice(0,-1)}.md`).mtime.toISOString();
+        }
+      }
+
+      return item;
+    }
+  })]
 });
