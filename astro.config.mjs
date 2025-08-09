4dd2433fd8305e6425f242e90828f872e168d8aa
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2025-08-09 00:51:52 +0000

    refactor: PageFooter styled; PageHeader improved
    In addition to simplifying and improving the styling of `PageFooter`,
    this commit also brings a complete revision of `PageHeader`, since I
    could better leverage the features of Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://heinrichtaver.com",
   image: {
     layout: "constrained",
     responsiveStyles: true,
