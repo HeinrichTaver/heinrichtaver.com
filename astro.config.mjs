aaf2316d09fa90f397f9ea067c4cb29a729ff4c6
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2025-08-10 13:27:48 +0000

    fix: site configured as www.heinrichtaver.com
    I don't know if this makes a difference, but Google Search Console seems
    sensitive to inconsistencies. Since the official URL of my website, as
    Vercel suggests, is now `www.heinrichtaver.com`, I changed it so that
    the Sitemap address is consistent with that.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://heinrichtaver.com",
+  site: "https://www.heinrichtaver.com",
 
   image: {
     layout: "constrained",
